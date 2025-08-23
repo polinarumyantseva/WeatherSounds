52e364cd20914d5f72239dbee86a869f4e09ea63
polinarumyantseva <polinrumyantseva@gmail.com>
2025-08-23 13:38:49 -0700

    feat: настраивает конфигурацию webpack

---
+++ webpack.config.common.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: './index.js',
+	output: {
+		filename: '[name].[contenthash].js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'public/assets'),
+					to: path.resolve(__dirname, 'dist/assets'),
+				},
+			],
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [require('postcss-preset-env')],
+							},
+						},
+					},
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(mp3)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: 'sounds/[name][ext]',
+				},
+			},
+		],
+	},
+};
